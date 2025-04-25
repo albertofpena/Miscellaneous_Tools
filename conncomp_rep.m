5785359a2732b2a9fbd3e6179b751dff7a6418b2
Alberto F. Pena <alberto.fernandez.pena@gmail.com>
2025-04-25 12:39:29 +0200

    changed function name

--- conncomp_rep.m
+++ conncomp_rep.m
@@ -1,4 +1,4 @@
-function [S,C] = conncomp(G)
+function [S,C] = conncomp_rep(G)
   % CONNCOMP Drop in replacement for graphconncomp.m from the bioinformatics
   % toobox. G is an n by n adjacency matrix, then this identifies the S
   % connected components C. This is also an order of magnitude faster.
